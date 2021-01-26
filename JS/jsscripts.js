a469ccdc375b387a21de8544a24adf81542c72d1
lordaries <77642556+lordaries@users.noreply.github.com>
2021-01-26 13:05:57 -0800

    Paused coding for JS Rocket

--- JS/jsscripts.js
+++ JS/jsscripts.js
@@ -1,7 +1,27 @@
+setTimeout(function () {
+	location.href = 'rocket.html';
+}, 2000);
+
+// this function does this.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	// changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
+var sayMyName = function (name) {
+	alert('My name is: '+name);
+}
+
 var car = {
 	make: 'VW',
 	type: 'Polo',
-	color: 'Blue',
+	color: 'blue',
 	isTurnedOn: false,
 	numberOfWheels: 4,
 	seats: [
@@ -9,23 +29,23 @@ var car = {
 		'seat 2',
 		'seat 3',
 		'seat 4'
-		],
-		turnOn: function () {
+	],
+	turnOn: function () {
+		this.isTurnedOn = true;
+	},
+	fly: function () {
+		alert('fly');
+	},
+	switchCar: function (isOn) {
+		console.log('turn car '+isOn);
+		if (isOn == true) {
 			this.isTurnedOn = true;
-		},
-		fly: function () {
-			alert('fly');
-		},
-		switchCar: function (isOn) {
-			console.log ('turn car '+isOn)
-				;
-			if (isOn == true) {
-				this.isTurnedOn = true;
-			} else {
-				this.isTurnedOn = false;
-			}
-
+		} else {
+			this.isTurnedOn = false;
 		}
+	}
 };
 
-console.log('hello there friends!');	
+console.log('hello there friends!');
+
+console.log('hello there friends!');
